0a2d8adb85276b341941df14aa8210a82462f50c
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2010-12-09 00:26:42 +0100

    Check for git directory using git-rev-parse.

--- home/.my-bash/theme/base.bash
+++ home/.my-bash/theme/base.bash
@@ -20,7 +20,7 @@ RVM_THEME_PROMPT_PREFIX=' |'
 RVM_THEME_PROMPT_SUFFIX='|'
 
 function scm {
-  if [[ -d .git ]]; then SCM=$GIT
+  if [[ -n $(git rev-parse --git-dir 2>/dev/null) ]]; then SCM=$GIT
   elif [[ -d .hg ]]; then SCM=$HG
   elif [[ -d .svn ]]; then SCM=$SVN
   else SCM='NONE'
