10e8b5d40ae5214900f2c71e11f10912f0ce8c42
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2010-12-11 22:12:59 +0100

    Non-printable characters were breaking the prompt.
    Leave color theme a little more dull while I find a solution.

--- home/.my-bash/theme/theme.bash
+++ home/.my-bash/theme/theme.bash
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-SCM_THEME_PROMPT_DIRTY=" ${red}✗"
-SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
-SCM_THEME_PROMPT_PREFIX=" ${green}|"
-SCM_THEME_PROMPT_SUFFIX="${green}|"
+# SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+# SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+# SCM_THEME_PROMPT_PREFIX=" ${green}|"
+# SCM_THEME_PROMPT_SUFFIX="${green}|"
 
 
-PROMPT="\[${bold_blue}\]\[\$(scm_char)\]\[${green}\]\[\$(scm_prompt_info)\]\[${blue}\]\[\$(rvm_version_prompt)\]\[${reset_color}\] \h:\W \u\$ "
+PROMPT="\[${bold_blue}\]\$(scm_char)\[${green}\]\$(scm_prompt_info)\[${blue}\]\$(rvm_version_prompt)\[${reset_color}\] \h:\W \u\$ "
