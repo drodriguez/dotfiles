a46527e482a2c9b63d0a0018aea610b1662ff3c0
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2011-08-31 10:50:05 +0200

    Add known hosts to SSH completion.

--- home/.my-bash/completion/ssh.bash
+++ home/.my-bash/completion/ssh.bash
@@ -4,10 +4,21 @@
 export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
 
 _sshcomplete() {
-    if [ -f $HOME/.ssh/config ]; then
-        COMPREPLY=($(compgen -W "`grep '^Host' $HOME/.ssh/config | sed 's/^Host //' | sort | uniq`" -- ${COMP_WORDS[COMP_CWORD]}))
-        return 0
-    fi
+    COMPREPLY=()
+    comp_ssh_hosts=`cat $HOME/.ssh/known_hosts | \
+        cut -f 1 -d ' ' | \
+        sed -e s/,.*//g | \
+        grep -v ^# | \
+        uniq | \
+        grep -v "\[" ;
+        if [ -f $HOME/.ssh/config ]; then
+            cat $HOME/.ssh/config | \
+                grep "^Host " | \
+                awk '{print $2}'
+        fi
+        `
+    COMPREPLY=($(compgen -W "${comp_ssh_hosts}" -- ${COMP_WORDS[COMP_CWORD]}))
+    return 0
 }
 
 complete -o default -o nospace -F _sshcomplete ssh
