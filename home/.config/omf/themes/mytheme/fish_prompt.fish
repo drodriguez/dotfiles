8ce4ba584b6a55ef3cd18424c7f66a943618bf40
Daniel Rodríguez Troitiño <danielrodriguez@fb.com>
2022-01-24 19:07:39 -0800

    Fish: remove usage of caret for stderr redirection

--- home/.config/omf/themes/mytheme/fish_prompt.fish
+++ home/.config/omf/themes/mytheme/fish_prompt.fish
@@ -16,10 +16,10 @@ function fish_prompt
   set -l none     "◦"
 
   set -l normal_color     (set_color normal)
-  set -l success_color    (set_color $fish_pager_color_progress ^/dev/null; or set_color cyan)
-  set -l error_color      (set_color $fish_color_error ^/dev/null; or set_color red --bold)
-  set -l directory_color  (set_color $fish_color_quote ^/dev/null; or set_color brown)
-  set -l repository_color (set_color $fish_color_cwd ^/dev/null; or set_color green)
+  set -l success_color    (set_color $fish_pager_color_progress 2>/dev/null; or set_color cyan)
+  set -l error_color      (set_color $fish_color_error 2>/dev/null; or set_color red --bold)
+  set -l directory_color  (set_color $fish_color_quote 2>/dev/null; or set_color brown)
+  set -l repository_color (set_color $fish_color_cwd 2>/dev/null; or set_color green)
   set -l python_color     (set_color -b blue white)
 
   if test $last_command_status -eq 0
@@ -30,7 +30,7 @@ function fish_prompt
 
   if git_is_repo
     if test "$theme_short_path" = 'yes'
-      set root_folder (command git rev-parse --show-toplevel ^/dev/null)
+      set root_folder (command git rev-parse --show-toplevel 2>/dev/null)
       set parent_root_folder (dirname $root_folder)
       set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
 
