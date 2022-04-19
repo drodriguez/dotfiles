a72485677434e31b8c27913edc388e28c4ae6da4
Daniel Rodríguez Troitiño <danielrodriguez@fb.com>
2022-04-19 10:33:57 -0700

    Hammerspoon: Caffeinate menubar

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,4 +1,3 @@
-
 -- A global variable for the Hyper Mode
 k = hs.hotkey.modal.new({}, "F17")
 
@@ -134,3 +133,62 @@ for _, keyFn in pairs(functionBindings) do
     k.triggered = true
   end)
 end
+
+-- Caffeine menubar
+caffeine = hs.menubar.new()
+
+-- Always caffeinate on startup.
+shouldCaffeinate = false
+
+function setCaffeineDisplay(state)
+  if state then
+    caffeine:setTitle("☕️")
+  else
+    caffeine:setTitle("💤")
+  end
+end
+
+function setCaffeine(state)
+  hs.caffeinate.set("displayIdle", state, true)
+  setCaffeineDisplay(state)
+end
+
+function caffeineClicked()
+  shouldCaffeinate = not shouldCaffeinate
+  setCaffeine(shouldCaffeinate)
+end
+
+if caffeine then
+  caffeine:setClickCallback(caffeineClicked)
+  setCaffeine(shouldCaffeinate)
+end
+
+local pow = hs.caffeinate.watcher
+local log = hs.logger.new("caffeine", 'verbose')
+
+local function on_pow(event)
+  local name = "?"
+  for key,val in pairs(pow) do
+    if event == val then name = key end
+  end
+  log.f("caffeinate event %d => %s", event, name)
+  if event == pow.screensDidUnlock
+    or event == pow.screensaverDidStop
+  then
+    log.i("Screen awakened!")
+    -- Restore Caffeinated state:
+    setCaffeine(shouldCaffeinate)
+    return
+  end
+  if event == pow.screensDidLock
+    or event == pow.screensaverDidStart
+  then
+    log.i("Screen locked.")
+    setCaffeine(false)
+    return
+  end
+end
+
+-- Listen for power events, callback on_pow().
+pow.new(on_pow):start()
+log.i("Started.")
