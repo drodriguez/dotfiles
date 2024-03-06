b77710e6a72499dd0436b3dff31d5a469d25c3af
Daniel Rodríguez Troitiño <danielrodriguez@meta.com>
2024-03-05 18:04:22 -0800

    Fish uses 2> not ^. Old files keep reappering.

--- home/.config/fish/functions/hg_current_name.fish
+++ home/.config/fish/functions/hg_current_name.fish
@@ -1,7 +1,7 @@
 function hg_current_name -d "Get Mercurial current name (bookmark/branch)"
   hg_is_repo; and begin
-    set -l branch (command hg branch ^/dev/null)
-    set -l bookmark (command hg bookmark | command grep \* | cut -d\  -f3)
+    set -l branch (command hg branch 2> /dev/null)
+    set -l bookmark (command hg bookmark 2> /dev/null | command grep \* | cut -d\  -f3)
 
     if [ "$bookmark" != '' ]
       echo "$branch @ $bookmark"
--- home/.config/fish/functions/hg_is_repo.fish
+++ home/.config/fish/functions/hg_is_repo.fish
@@ -1,3 +1,3 @@
 function hg_is_repo -d "Check if directory is a Mercurial repository"
-  test -d .hg; or command hg root > /dev/null ^/dev/null
+  test -d .hg; or command hg root > /dev/null 2> /dev/null
 end
