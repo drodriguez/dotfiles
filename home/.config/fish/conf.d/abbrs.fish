919f20003b12026c5747193abf44368ae9e41c1f
Daniel Rodríguez Troitiño <danielrodriguez@meta.com>
2025-02-26 15:46:58 -0800

    Fish and Git updates
    - Remove the abbrs_initialized from Fish, since abbr doesn't seem to be
      shared between shells anymore.
    - Enable iTerm integration for Fish. Maybe not as slow as I remember.
    - Enable Homebrew integration (with existence check)
    - Load Fish completions from /etc/fish/completions
    
    - New abbreviations for Git stash commands
    - Enable LFS

--- home/.config/fish/conf.d/abbrs.fish
+++ home/.config/fish/conf.d/abbrs.fish
@@ -1,25 +1,21 @@
-if not set -q abbrs_initialized
-  abbr -a be bundle exec
-  abbr -a f open -a Finder
-  abbr -a g git
-  abbr -a ga git add
-  abbr -a gap git add --patch
-  abbr -a gc git commit
-  abbr -a gcb git checkout -b
-  abbr -a gco git checkout
-  abbr -a gd git diff
-  abbr -a gdc git diff --cached
-  abbr -a gl git lg
-  abbr -a gmt git mergetool
-  abbr -a gpl git pull
-  abbr -a grs git reset
-  abbr -a grc git rebase --continue
-  abbr -a gs git status
-  abbr -a ls eza
-  abbr -a ll eza -al
-  abbr -a cat bat
-  abbr -a mategem gem edit -e mate
-  abbr -a pi bundle exec pod install
+abbr -a be bundle exec
+abbr -a f open -a Finder
+abbr -a g git
+abbr -a ga git add
+abbr -a gap git add --patch
+abbr -a gc git commit
+abbr -a gcb git checkout -b
+abbr -a gco git checkout
+abbr -a gd git diff
+abbr -a gdc git diff --cached
+abbr -a gl git lg
+abbr -a gmt git mergetool
+abbr -a gpl git pull
+abbr -a grs git reset
+abbr -a grc git rebase --continue
+abbr -a gs git status
+abbr -a ls eza
+abbr -a ll eza -al
+abbr -a cat bat
+abbr -a pi bundle exec pod install
 
-  set -U abbrs_initialized
-end
--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,10 +2,16 @@ if type -q rbenv
     status --is-interactive; and source (rbenv init -|psub)
 end
 
-# test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+test -e /opt/homebrew/bin/brew ; and eval (/opt/homebrew/bin/brew shellenv)
 
 # eval (python -m virtualfish)
 
 direnv hook fish | source
 
 zoxide init --cmd cd fish | source
+
+for file in /etc/fish/completions/*.fish
+  source $file
+end
